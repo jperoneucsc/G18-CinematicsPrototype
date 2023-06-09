6efffc808b2c49c8d0ef9b22e30b6abbeedcf142
Schrodawg <cjschroe@ucsc.edu>
2023-06-09 12:10:01 -0700

    audio added, tween chain added, readME updated

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ CMPM 120 Final Project - Group 18 Cinematic Prototype
 
 This prototype displays an non-interactive cinematic, an interactive cinematic, and uses tween chains for code choreography.
 
-The intro "logo" scene features our imaginary game company's logo. It fades in, 
+The intro "logo" scene features our imaginary game company's logo. It fades in, hovers a bit, does a 360 degree rotation, a skateboard sound effect plays and then it fades out to black.
 
-All scenes are reachable and all have a fade transition. 
+The "title" scene then plays. Here we have some simple looping animations to give off that "alive" appearance. The "play" and "credits" buttons scale down their respective text when pressed or hovered over.
+
+The "logo" scene also features a tween chain. All animations are conducted through the tween chain-based variable "tween".
+
+skate sfx: https://pixabay.com/sound-effects/skateboard-rolling-96991/
--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -6,12 +6,18 @@ class Intro extends Phaser.Scene {
     preload(){
         this.load.image("background", "src/assets/titleMenu/menuBackground.png");
         this.load.image("logo", "src/assets/titleMenu/logo.png")
+        this.load.audio("board", ['src/assets/titleMenu/skateboard_rolling.mp3']);
     }
 
     create() {
 
         let scaleRatio = window.devicePixelRatio / 3;
         this.cameras.main.fadeIn(2000);
+        let skateSound = this.sound.add('board',
+        {
+            volume: .15, 
+            delay: 1
+        });
 
 
         let background = this.add.image(0, 0, 'background').setOrigin(0,0).setInteractive();
@@ -20,16 +26,32 @@ class Intro extends Phaser.Scene {
         var style = { font: "Bold 132px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         var style2 = { font: "Bold 42px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         // add title and movement anim
-        let title = this.add.image(960, 490, 'logo').setOrigin(0,0).setInteractive().setOrigin(.5,.5).setScale(scaleRatio * 50, scaleRatio * 40);
-        var tween = this.tweens.add({
-            targets: title,
-            y: { start: 500, from: 500, to: 400},
-            ease:'Linear',
-            yoyo: true,
-            repeat: 1,
-            duration: 4000,
-            onComplete: () => {this.cameras.main.fadeOut(2000)}
+        let logo = this.add.image(960, 490, 'logo').setOrigin(0,0).setInteractive().setOrigin(.5,.5).setScale(scaleRatio * 40, scaleRatio * 30);
+        // tween chain
+        var tween = this.tweens.chain({
+            targets: logo,
+            tweens: [
+                {
+                y: { start: 500, from: 500, to: 400},
+                ease:'Linear',
+                yoyo: true,
+                repeat: 1,
+                delay: 500,
+                duration: 2000,
+                },
+                {
+                    angle: 360,
+                    ease: 'expo.out',
+                    duration: 500,
+                    onComplete: () => {
+                        skateSound.play();
+                        this.cameras.main.fadeOut(2000);
+                        
+                    }
+                }
+            ]    
         })
+        tween.play();
     
         this.cameras.main.on('camerafadeoutcomplete', () => {
             this.scene.start('Title');
